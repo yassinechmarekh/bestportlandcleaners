7862cb6f513dd377c5133058a505a9bc75114188
yassine <yassinechmarekh88@gmail.com>
2024-11-09 13:14:17 +0100

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import About from "./pages/website/About";
+import Home from "./pages/website/Home";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Services from "./pages/website/Services";
+import OurWorks from "./pages/website/OurWorks";
+import Contact from "./pages/website/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./pages/admin/Login";
+import Dashboard from "./pages/admin/Dashboard";
+import NotFound from "./pages/website/NotFound";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Outlet />
+                <Footer />
+              </>
+            }
+          >
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="services" element={<Services />} />
+            <Route path="ourworks" element={<OurWorks />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/website/OurWorks.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OurWorks() {
+  return (
+    <div>OurWorks</div>
+  )
+}
